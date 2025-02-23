180ad1109909f2060c4b9001b7b85560db129a5a
Princegaming9026 <93808464+Princegaming9026@users.noreply.github.com>
2025-02-23 14:37:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-function playMedia() {
-    const video = document.getElementById('videoPlayer');
+document.addEventListener("DOMContentLoaded", function () {
+    function playMedia() {
+        const video = document.getElementById("videoPlayer");
 
-    if (Hls.isSupported()) {
-        const hls = new Hls();
-        hls.loadSource(STREAM_URL);
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED, function () {
+        if (Hls.isSupported()) {
+            const hls = new Hls();
+            hls.loadSource(STREAM_URL);
+            hls.attachMedia(video);
+            hls.on(Hls.Events.MANIFEST_PARSED, function () {
+                video.play();
+            });
+        } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+            video.src = STREAM_URL;
             video.play();
-        });
-    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-        video.src = STREAM_URL;
-        video.play();
+        }
     }
-}
 
-// Hide loading and show video after 3 sec
-setTimeout(() => {
-    document.getElementById('loading').style.display = 'none';
-    document.getElementById('videoContainer').style.display = 'block';
-    playMedia();
-}, 3000);
+    // Hide loading and show video after 3 sec
+    setTimeout(() => {
+        document.getElementById("loading").style.display = "none";
+        document.getElementById("videoContainer").style.display = "block";
+        playMedia();
+    }, 3000);
+});
