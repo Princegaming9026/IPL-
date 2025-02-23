addc7bf6123c6aac7439f57336f6d8efa177aa79
Princegaming9026 <93808464+Princegaming9026@users.noreply.github.com>
2025-02-23 15:32:55 +0530

    Update stream.php

--- stream.php
+++ stream.php
@@ -15,7 +15,7 @@ header("Content-Type: application/vnd.apple.mpegurl");
 header("Access-Control-Allow-Origin: *");
 
 // Secure M3U8 link (Replace with your actual link)
-$m3u8_url = "http://link.luckonline.eu/live/0c:89:10:e8:1c:d4/869/505.m3u8";
+$m3u8_url = "https://hocdn.news/live/starhindi.m3u8";
 
 // Stream the M3U8 file
 echo file_get_contents($m3u8_url);
