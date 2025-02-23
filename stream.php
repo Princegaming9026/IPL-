496d7aa61c5ba4ae5d51f46322af030cd6392865
Princegaming9026 <93808464+Princegaming9026@users.noreply.github.com>
2025-02-23 16:04:58 +0530

    Update stream.php

--- stream.php
+++ stream.php
@@ -15,7 +15,7 @@ header("Content-Type: application/vnd.apple.mpegurl");
 header("Access-Control-Allow-Origin: *");
 
 // Secure M3U8 link (Replace with your actual link)
-$m3u8_url = "https://hocdn.news/live/starhindi.m3u8";
+$m3u8_url = "https://jcevents.jiocinema.com/bpk-tv/JC_Sports18_1HD/JCHLS/hdntl=exp=1740378932~acl=%2F*~id=aaf549ec51984c86b32dd7de3e1334c7~data=hdntl~hmac=6029ae6ff841ccbb41d820a46f0a255ad02a678707e7c6ad9a63bd546d3ee24d/JC_Sports18_1HD-audio_108038_eng=108000-video=2297600.m3u8";
 
 // Stream the M3U8 file
 echo file_get_contents($m3u8_url);
